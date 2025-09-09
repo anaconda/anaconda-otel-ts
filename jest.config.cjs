62d859b1a63846484706b2c7e473bf0e3ac30784
Paul Amonson <pamonson@anaconda.com>
2025-09-09 11:37:55 -0700

    [fix] More added files, attempt to fix CI.
    Signed-off-by: Paul Amonson <pamonson@anaconda.com>

--- jest.config.cjs
+++ jest.config.cjs
@@ -12,13 +12,15 @@ module.exports = {
       displayName: 'unit',
       preset: 'ts-jest',
       testEnvironment: 'node',
+      extensionsToTreatAsEsm: ['.ts'],
       testMatch: ['<rootDir>/tests/unit/**/*.(spec|test).ts'],
       moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1'
       },
       transform: {
-        ...tsJestTransformCfg,
+        '^.+\\.ts$': ['ts-jest', { useESM: true }]
       },
+      collectCoverageFrom: ['src/**/*.ts'],
       coveragePathIgnorePatterns: [
         "/node_modules/",
         "/integration_tests/"
@@ -28,6 +30,7 @@ module.exports = {
       displayName: 'integration',
       preset: 'ts-jest',
       testEnvironment: 'node',
+      extensionsToTreatAsEsm: ['.ts'],
       testMatch: ['<rootDir>/tests/integration/**/*.(spec|test|e2e).ts'],
       moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1'
